0f03586eaf88749e2ddad13e034f07cf52e5200c
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-06-29 22:29:36 +0900

    Create parameter.php

---
+++ parameter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <!-- 안녕하세요. egoing님 -->
+    <!-- 안녕하세요. <?php echo $_GET['name']; ?>님 -->
+    <!-- http://localhost/parameter.php?name=egoing -->
+
+    안녕하세요. <?php echo $_GET['address']; ?>에 사시는 <?php echo $_GET['name']; ?>님
+    <!-- http://localhost/parameter.php?name=egoing&adress=서울 -->
+  </body>
+</html>

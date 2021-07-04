291dc66bf14e52820938fa00586550c15dfe259c
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-04 11:52:08 +0900

    Create index.php

---
+++ [ch18]FunctionPrequel/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <?php
+        print_title();
+      ?>
+    </title>
+  </head>
+  <body>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+      <?php
+        print_list();
+      ?>
+    </lo>
+    <h2>
+      <?php
+        print_title();
+      ?>
+    </h2>
+    <?php
+      print_description();
+    ?>
+  </body>
+</html>

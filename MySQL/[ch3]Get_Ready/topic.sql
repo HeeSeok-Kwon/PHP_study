5aba59a17cb6a88b19e922ae815fadf314f5913b
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-07 22:39:22 +0900

    Create topic.sql

---
+++ MySQL/[ch3]Get_Ready/topic.sql
@@ -0,0 +1,14 @@
+# CREATE DATABASE opentutorials;
+# SHOW DATABASES;
+# USE opentutorials;
+
+CREATE TABLE topic (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  title varchar(45) NOT NULL,
+  description text,
+  created datetime NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE = InnoDB;
+
+# SHOW TABLES;
+# DESC topic;

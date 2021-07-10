1d2630187db133546a21d72dc8f155fd9b4ecc54
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-10 21:06:22 +0900

    Create create.php

---
+++ MySQL/[ch9]Read_A_Post/create.php
@@ -0,0 +1,46 @@
+<?php
+  $mysqli = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+  $sql = "SELECT * FROM topic";
+  $result = mysqli_query($mysqli, $sql);
+  $list = '';
+  // Title 목록 코드
+  while($row = mysqli_fetch_array($result)) {
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  // Description 코드
+  $article = array(
+    'title'=> 'Welcome',
+    'description'=>'Hello, web'
+    );
+  if(isset($_GET['id'])) {
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($mysqli, $sql);
+  $row = mysqli_fetch_array($result);
+  $article = array(
+    'title'=> $row['title'],
+    'description'=>$row['description']
+    );
+  }
+  // print_r($article)
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <!-- <li>HTML</li> -->
+      <?= $list ?>
+    </ol>
+    <form action="process_create.php" method="POST">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p><textarea name="description" placeholder="description"></textarea></p>
+      <p><input type="submit"></p>
+    </form>
+  </body>
+</html>

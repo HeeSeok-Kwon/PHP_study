0b612fc41a6180834177288dfba2b3bbcf65ea9e
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-10 21:54:18 +0900

    Create process_create.php

---
+++ MySQL/[ch10]Security/SQL_injection/process_create.php
@@ -0,0 +1,35 @@
+<?php
+  // var_dump($_POST);
+
+  $mysqli = mysqli_connect('localhost', 'user', '******', 'opentutorials');
+  $filtered = array (
+    'title'=>mysqli_real_escape_string($mysqli, $_POST['title']),
+    'description'=>mysqli_real_escape_string($mysqli, $_POST['description'])
+  );
+
+  // mysqli_real_escape_string 제거
+  // $filtered = array (
+  //   'title'=>($_POST['title']),
+  //   'description'=>($_POST['description'])
+  // );
+
+  $sql = "
+    INSERT INTO topic
+      (title, description, created)
+      VALUES(
+        '{$filtered['title']}',
+        '{$filtered['description']}',
+        NOW()
+        )";
+
+  // die($sql);
+
+  $result = mysqli_multi_query($mysqli, $sql);
+  if($result === false) {
+    echo "저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.";
+    error_log(mysqli_error($mysqli));
+  } else {
+    echo '성공했습니다. <a href="index.php">돌아가기</a>';
+  }
+  // echo $sql;
+?>

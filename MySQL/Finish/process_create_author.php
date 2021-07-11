b739c8901373204220dda4164c59fd8732e6ccb0
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-11 10:59:54 +0900

    Update process_create_author.php

--- MySQL/Finish/process_create_author.php
+++ MySQL/Finish/process_create_author.php
@@ -1,5 +1,5 @@
 <?php
-  $mysqli = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+  $mysqli = mysqli_connect('localhost', 'user', '******', 'opentutorials');
   $filtered = array (
     'name'=>mysqli_real_escape_string($mysqli, $_POST['name']),
     'profile'=>mysqli_real_escape_string($mysqli, $_POST['profile'])

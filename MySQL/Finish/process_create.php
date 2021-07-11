42e326ae472fcfb54914673ca9f58ab57765e51c
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-11 10:59:37 +0900

    Update process_create.php

--- MySQL/Finish/process_create.php
+++ MySQL/Finish/process_create.php
@@ -1,7 +1,7 @@
 <?php
   // var_dump($_POST);
 
-  $mysqli = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+  $mysqli = mysqli_connect('localhost', 'user', '******', 'opentutorials');
   // print_r($_POST);
   $filtered = array (
     'title'=>mysqli_real_escape_string($mysqli, $_POST['title']),

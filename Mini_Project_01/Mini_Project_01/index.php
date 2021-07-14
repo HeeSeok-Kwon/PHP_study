04aa2a67412ce9152ec9133b54eb5ed8ed69902d
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-14 09:26:54 +0900

    Add files via upload

---
+++ Mini_Project_01/Mini_Project_01/index.php
@@ -0,0 +1,104 @@
+<?php
+  $mysqli = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+?>
+
+<?php
+  // singer code
+  $sql = "SELECT * FROM singer";
+  $result = mysqli_query($mysqli, $sql);
+  $select_form = '<select name="singer_id">';
+  while($row = mysqli_fetch_array($result)) {
+    $select_form .= '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+  }
+  $select_form .= '</select>';
+  // update
+  $escaped = array(
+    'id'=>'',
+    'title'=>'',
+    'album'=>'',
+    'likes'=>'',
+    'singer_id'=>''
+  );
+  $header2 = "Register Form";
+  $label_submit = 'Register music';
+  $form_action = "process_register_music.php";
+  $form_id = '<input type="text" name="id" placeholder="ID">';
+  if(isset($_GET['id'])) {
+    $filtered_id = mysqli_real_escape_string($mysqli, $_GET['id']);
+    // echo $filtered_id;
+    settype($filtered_id, 'integer');
+    $sql = "SELECT * FROM music WHERE id={$filtered_id}";
+    // query
+    $result = mysqli_query($mysqli, $sql);
+    $row = mysqli_fetch_array($result);
+    $escaped['id'] = htmlspecialchars($row['id']);
+    $escaped['title'] = htmlspecialchars($row['title']);
+    $escaped['album'] = htmlspecialchars($row['album']);
+    $escaped['likes'] = htmlspecialchars($row['likes']);
+    // echo $escaped['likes'];
+    $escaped['singer_id'] = htmlspecialchars($row['singer_id']);
+    // 보이는 제목 바꾸기
+    $header2 = "Update Form";
+    $label_submit = 'Update music';
+    $form_action = "process_update_music.php";
+    $form_id = '<input type="text" readonly name="id" value="'.$escaped['id'].'">';
+    // echo $escaped['id'];
+    $select_form = '<input type="text" readonly name="singer_id" value="'.$escaped['singer_id'].'">';
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><a href="index.php">Music</a></h1>
+    <table border=1>
+      <tr>
+        <th>ID</th><th>Title</th><th>Album</th><th>Likes</th><th>singer_ID</th>
+      </tr>
+      <?php
+        $sql = "SELECT * FROM music";
+        $result = mysqli_query($mysqli, $sql);
+        while($row = mysqli_fetch_array($result)) {
+          $filtered = array(
+            'id'=>htmlspecialchars($row['id']),
+            'title'=>htmlspecialchars($row['title']),
+            'album'=>htmlspecialchars($row['album']),
+            'likes'=>htmlspecialchars($row['likes']),
+            'singer_id'=>htmlspecialchars($row['singer_id'])
+          );
+          ?>
+          <tr>
+            <td><?= $filtered['id'] ?></td>
+            <td><?= $filtered['title'] ?></td>
+            <td><?= $filtered['album'] ?></td>
+            <td><?= $filtered['likes'] ?></td>
+            <td><?= $filtered['singer_id'] ?></td>
+            <td><a href="index.php?id=<?= $filtered['id']?>">update</a></td>
+            <td>
+              <form action="process_delete_author.php" method="post" onsubmit="if(!confirm('sure')){return false;}">
+                <input type="hidden" name="id" value=<?= $filtered['id'] ?>>
+                <input type="submit" value="delete">
+              </form>
+            </td>
+          </tr>
+          <?php
+        }
+      ?>
+    </table>
+
+    <h2><?= $header2 ?></h2>
+    <form action="<?= $form_action ?>" method="post">
+      <!-- <p><input type="text" name="id" placeholder="ID"></p> -->
+      <p><?= $form_id ?></p>
+      <p><input type="text" name="title" placeholder="Title" value="<?= $escaped['title'] ?>"></p>
+      <p><input type="text" name="album" placeholder="Album" value="<?= $escaped['album'] ?>"></p>
+      <p><input type="text" name="likes" placeholder="Likes" value="<?= $escaped['likes'] ?>"></p>
+      <?= $select_form ?>
+      <p><input type="submit" value="<?= $label_submit ?>"></p>
+    </form>
+  </body>
+</html>
---
+++ Mini_Project_01/Mini_Project_01/process_register_music.php
@@ -0,0 +1,34 @@
+<?php
+  // var_dump($_POST);
+
+  $mysqli = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+  // print_r($_POST);
+  $filtered = array (
+    'id'=>mysqli_real_escape_string($mysqli ,$_POST['id']),
+    'title'=>mysqli_real_escape_string($mysqli ,$_POST['title']),
+    'album'=>mysqli_real_escape_string($mysqli ,$_POST['album']),
+    'likes'=>mysqli_real_escape_string($mysqli ,$_POST['likes']),
+    'singer_id'=>mysqli_real_escape_string($mysqli ,$_POST['singer_id'])
+  );
+
+  $sql = "
+    INSERT INTO music
+      (id, title, album, likes, singer_id)
+      VALUES(
+        '{$filtered['id']}',
+        '{$filtered['title']}',
+        '{$filtered['album']}',
+        '{$filtered['likes']}',
+        '{$filtered['singer_id']}'
+        )";
+  // die($sql);
+  $result = mysqli_multi_query($mysqli, $sql);
+  if($result === false) {
+    echo "저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.";
+    error_log(mysqli_error($mysqli));
+  } else {
+    // echo '성공했습니다. <a href="index.php">돌아가기</a>';
+    header('Location: index.php');
+  }
+  // echo $sql;
+?>

416bfd0701b5fb77f9ed8d53b9f87771abac1afe
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-07-14 09:28:10 +0900

    Update process_delete_author.php

--- Mini_Project_01/process_delete_author.php
+++ Mini_Project_01/process_delete_author.php
@@ -1,5 +1,5 @@
 <?php
-  $mysqli = mysqli_connect('localhost', 'root', 'root', 'opentutorials');
+  $mysqli = mysqli_connect('localhost', 'user', '******', 'opentutorials');
   settype($_POST['id'], 'integer');
   $filtered = array (
     'id'=>mysqli_real_escape_string($mysqli, $_POST['id'])
